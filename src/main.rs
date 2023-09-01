643f90c2bdef0929d57820c157e3219267cd0cfe
Erik-TS <erik.t3600@hotmail.com>
2023-09-01 16:05:38 -0300

    Add file creator function.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::fs::File;
 use std::io::Error;
 use std::ops::Add;
 use std::path::Path;
@@ -8,13 +9,25 @@ fn main() {
     let start_dir = Path::new("/");
 }
 
-fn remove_file(path: &Path, file_name: &str) -> Result<String, Error>{
+fn create_file(path: &Path, file_name: &str) -> Result<String, Error> {
+    let new_file_path = path.join(Path::new(file_name));
+
+    if !new_file_path.exists() {
+        return match File::create(new_file_path) {
+            Ok(_) => Ok(String::from("File created with success.")),
+            Err(err) => Err(err),
+        };
+    }
+    Ok(String::from("File already exists."))
+}
+
+fn remove_file(path: &Path, file_name: &str) -> Result<String, Error> {
     let full_path_string = format!("{}/{}", path.to_string_lossy(), file_name);
     let removed_file_path = Path::new(full_path_string.as_str());
 
-    match fs::remove_file(removed_file_path){
+    match fs::remove_file(removed_file_path) {
         Ok(_) => Ok(String::from("File removed with success.")),
-        Err(err) => Err(err)
+        Err(err) => Err(err),
     }
 }
 
@@ -33,10 +46,14 @@ fn change_current_dir(dir: &Path) -> Result<String, Error> {
             let success_message: String;
 
             match dir.to_str() {
-                Some(dir_str) => success_message = String::from("Current directory: ").clone().add(dir_str),
-                None => success_message = String::from("Current directory changed! Failed printing it.")
+                Some(dir_str) => {
+                    success_message = String::from("Current directory: ").clone().add(dir_str)
+                }
+                None => {
+                    success_message = String::from("Current directory changed! Failed printing it.")
+                }
             }
-            
+
             Ok(success_message)
         }
         Err(err) => Err(err),
